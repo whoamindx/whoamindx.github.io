866b5bd0ffb740da927ef36311d40c8f4c9328c1
whoamindx <gabriel.ndr.sky@gmail.com>
2025-01-28 19:34:40 -0300

    chore(SSL): @vitejs/plugin-basic-ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://whoamindx.github.io',
   prefetch: {
     defaultStrategy: 'viewport'
+  },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
   }
 });
